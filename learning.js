b2a0260c4b1a6d2de2618e0e61d0ce1762b75efd
Lou Cairns <loucairns907@gmail.com>
2023-03-27 15:08:54 +0100

    Update / addition of lesson 8 js file

---
+++ learning.js
@@ -0,0 +1,20 @@
+function sum(){
+    let x = 2 + 2;
+    console.log(x);
+}
+
+const anotherSum = function (){
+    let x = 3 * 3
+    console.log(x);
+}
+
+function betsSum(param1 + param2) {
+    let x = param1 + param2;
+    return x;
+}
+
+bestSum(3, 4);
+bestSum (10, 20);
+bestSum(4, 5);
+console.log(x)
+
---
+++ scripts.js
@@ -0,0 +1,43 @@
+//declare a variable which can be changed
+//name the variable myName
+//set the value of the variable to a string which is your name
+//console.log(myName)
+
+
+let myName = "Lou"
+
+console.log(myName)
+
+// declare a variable that can be changed
+// give it a name of myAge
+// and assign the value to your age
+
+let myAge = "36"
+
+console.log(myAge)
+
+//declare a variable that can not be changed
+//name it nameAge
+//and give it a value that concatenate the two variable you created and logs in the console a sentence that reads: Example
+//hello my name is Chris and i am 40
+
+const nameAge = "Hello my name is "  + myName + " and I am " + myAge 
+
+console.log(nameAge)
+
+//declare a variable that can not be changed name y
+//assign a value of the number 5 to x
+//assign a value of the string 5 to y
+//declare a variable that can be changed named number and assign a value of x + y
+//console log what data type number is
+//You may need google
+//I want the console to tell me what data type the variable number is
+
+let X = 5
+const Y = "5"
+
+
+let Number = X + Y
+
+console.log(typeof Number)
+
